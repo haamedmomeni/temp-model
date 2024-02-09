b9cece608278900f55ea0c5d2c7e5f77aab8f33b
hamed <hmomeni@mro.nmt.edu>
2024-02-08 23:27:49 -0700

    adding all files

---
+++ src/app.py
@@ -0,0 +1,194 @@
+from dash import Dash, dcc, html, Input, Output, State
+import dash_bootstrap_components as dbc
+from io import BytesIO
+from zipfile import ZipFile
+
+from data_processing import load_csv, preprocess_data, split_train_test, get_column_list, smooth_data, filter_dataframe_by_hours
+from plotting import generate_scatter_plot, create_graph_div, update_graphs_and_predictions
+from get_ip import get_wired_interface_ip
+
+# Load data
+FILENAME = '2024-01-10.csv'
+raw_df = load_csv(FILENAME)
+# Preprocess data
+processed_df = preprocess_data(raw_df)
+processed_df.to_csv('processed_df.csv')
+# Split data into training and test datasets
+train_df, test_df = split_train_test(processed_df)
+train_df = smooth_data(train_df)
+test_df = smooth_data(test_df)
+
+# Assume col_list is a list of column names from your DataFrame
+col_list = get_column_list(processed_df)
+# Generate the options dynamically based on column names
+options = [{'label': col, 'value': f'SHOW_{col.upper().replace(" ", "_")}'} for col in col_list]
+
+html.Button('Save Data as CSV', id='save-csv-btn', n_clicks=0),
+dcc.Download(id='download-train-csv'),
+dcc.Download(id='download-test-csv'),
+
+
+# === Dash App Initialization ===
+app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+server = app.server
+
+# # === Dash Layout ===
+app.layout = html.Div([
+    html.H3('Predictive Model Dashboard'),
+
+    html.Button('Download Data as ZIP', id='download-zip-btn',
+                style={'display': 'block', 'margin': '10px', 'fontWeight': 'bold', "color": "white", "background-color": "#3B3B3B", "padding": "5px", "border-radius": "5px"}),
+    dcc.Download(id='download-zip'),
+
+    html.Div(style={'display': 'flex', 'flexWrap': 'wrap', 'justifyContent': 'space-between'}, children=[
+        # First column
+        html.Div([
+            html.Label('Temperatures:',
+                       style={'display': 'inline-block', 'margin-right': '10px', 'fontWeight': 'bold'}),
+
+            dcc.Checklist(
+                id='toggle-data',
+                options=options,
+                value=[],
+                style={'display': 'block', 'margin': '5px', 'fontWeight': 'bold'},
+                inputStyle={"margin-right": "5px", "cursor": "pointer"},
+                labelStyle={"display": "block", "margin": "5px", "color": "white", "background-color": "#3B3B3B", "padding": "5px", "border-radius": "5px"}
+            ),
+
+            html.Br(),
+
+            html.Div([
+                html.Label('Select Starting Hour:',
+                           style={'display': 'inline-block', 'margin-right': '10px',  'width': '150px'}),
+                dcc.Dropdown(
+                    id='start-hour-input',
+                    options=[{'label': f'{i} AM' if i < 12 else f'{i} PM', 'value': i} for i in range(24)],
+                    value=20,  # Default value
+                    clearable=False,
+                    style={'width': '80px', 'display': 'inline-block', 'color': 'black'}
+                ),
+            ], style={'display': 'flex', 'align-items': 'center', 'margin-right': '10px'}),
+
+            html.Br(),
+
+            html.Div([
+                html.Label('Select Ending Hour:',
+                           style={'display': 'inline-block', 'margin-right': '10px',  'width': '150px'}),
+                dcc.Dropdown(
+                    id='end-hour-input',
+                    options=[{'label': f'{i} AM' if i < 12 else f'{i} PM', 'value': i} for i in range(24)],
+                    value=6,  # Default value
+                    clearable=False,
+                    style={'width': '80px', 'display': 'inline-block', 'color': 'black'}
+                ),
+            ], style={'display': 'flex', 'align-items': 'center', 'margin-right': '10px'}),
+
+            html.Br(),
+
+        ], style={'width': '10%', 'display': 'inline-block', 'verticalAlign': 'top'}),
+
+        # Second column (Combining Second and Third rows)
+        html.Div([
+            html.Div(id='equation-display-2', style={'color': '#FF5349'}),
+            create_graph_div('Training Data (Differential Y motion)', 'train-diff-2-4',
+                             generate_scatter_plot(train_df['timestamp'].values,
+                                                   train_df['smoothed_difference_2_4'].values,
+                                                   'green', 'observation'),
+                             y_axis_title='Smoothed Difference 2-4'),
+            create_graph_div('Testing Data (Differential Y motion)', 'test-diff-2-4',
+                             generate_scatter_plot(test_df['timestamp'].values,
+                                                   test_df['smoothed_difference_2_4'].values,
+                                                   'purple', 'observation'),
+                             y_axis_title='Smoothed Difference 2-4'),
+            html.Div(id='equation-display-1', style={'color': '#FF5349'}),
+            create_graph_div('Training Data (Differential X motion)', 'train-diff-1-3',
+                             generate_scatter_plot(train_df['timestamp'].values,
+                                                   train_df['smoothed_difference_1_3'].values,
+                                                   'blue', 'observation'),
+                             y_axis_title='Smoothed Difference 1-3'),
+            create_graph_div('Testing Data (Differential X motion)', 'test-diff-1-3',
+                             generate_scatter_plot(test_df['timestamp'].values,
+                                                   test_df['smoothed_difference_1_3'].values,
+                                                   'red', 'observation'),
+                             y_axis_title='Smoothed Difference 1-3')
+        ], style={'width': '90%', 'display': 'inline-block'}),
+    ]),
+])
+
+
+@app.callback(
+    [Output('train-diff-1-3', 'figure'),
+     Output('test-diff-1-3', 'figure'),
+     Output('equation-display-1', 'children')],
+    [Input('toggle-data', 'value'),
+     Input('start-hour-input', 'value'),
+     Input('end-hour-input', 'value')]
+)
+def update_diff_1_3_graphs(toggle_value, start_hour, end_hour):
+    return update_graphs_and_predictions(toggle_value, start_hour, end_hour, train_df, test_df,
+                                         'smoothed_difference_1_3',
+                                         'Training Data: Smoothed Difference (Column 3 - Column 1)',
+                                         'Test Data: Smoothed Difference (Column 3 - Column 1)',
+                                         options)
+
+
+@app.callback(
+    [Output('train-diff-2-4', 'figure'),
+     Output('test-diff-2-4', 'figure'),
+     Output('equation-display-2', 'children')],
+    [Input('toggle-data', 'value'),
+     Input('start-hour-input', 'value'),
+     Input('end-hour-input', 'value')]
+)
+def update_diff_2_4_graphs(toggle_value, start_hour, end_hour):
+    return update_graphs_and_predictions(toggle_value, start_hour, end_hour, train_df, test_df,
+                                         'smoothed_difference_2_4',
+                                         'Training Data: Smoothed Difference (Column 4 - Column 2)',
+                                         'Test Data: Smoothed Difference (Column 4 - Column 2)',
+                                         options)
+
+
+@app.callback(
+    Output('download-zip', 'data'),
+    Input('download-zip-btn', 'n_clicks'),
+    State('toggle-data', 'value'),
+    State('start-hour-input', 'value'),
+    State('end-hour-input', 'value'),
+    prevent_initial_call=True
+)
+def generate_and_download_zip(n_clicks, toggle_value, start_hour, end_hour):
+    if n_clicks > 0:
+
+        trained_df_filtered = filter_dataframe_by_hours(train_df, start_hour, end_hour)
+        test_df_filtered = filter_dataframe_by_hours(test_df, start_hour, end_hour)
+
+        # Create a mapping from value to label
+        value_to_label = {option['value']: option['label'] for option in options}
+
+        # Filter columns based on selected options
+        selected_columns = (['timestamp'] +
+                            [value_to_label[value] for value in toggle_value] +
+                            ['smoothed_difference_1_3', 'smoothed_difference_2_4'])
+
+        # Create in-memory ZIP file
+        zip_buffer = BytesIO()
+        with ZipFile(zip_buffer, 'w') as zip_file:
+            # Convert train_df and test_df to CSV strings without index
+            train_csv = trained_df_filtered[selected_columns].dropna().to_csv(index=False)
+            test_csv = test_df_filtered[selected_columns].dropna().to_csv(index=False)
+
+            # Write CSV strings to the ZIP file
+            zip_file.writestr('train_data.csv', train_csv)
+            zip_file.writestr('test_data.csv', test_csv)
+
+        # Prepare the ZIP file to be sent to the client
+        zip_buffer.seek(0)
+        return dcc.send_bytes(zip_buffer.getvalue(), filename="data.zip")
+
+
+# === Run the App ===
+if __name__ == '__main__':
+    # ip_address = get_wired_interface_ip()
+    # app.run_server(debug=True, host=f"{ip_address}",
+    #                port=8050)
+    app.run_server(debug=True)
---
+++ src/data_processing.py
@@ -0,0 +1,150 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, max_error
+
+
+def load_csv(file_path):
+    df = pd.read_csv(file_path)
+    df.drop([0, 1], inplace=True)
+    df.dropna(inplace=True)
+    return df
+
+
+def preprocess_data(df):
+    for col in df.columns:
+        if df[col].dtype == 'object':
+            try:
+                df[col] = df[col].astype(float)
+            except ValueError:
+                pass
+                # print(f"Cannot convert column {col} to float.")
+
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+    df['date'] = df['timestamp'].dt.date
+
+    df['difference_1_3'] = df.iloc[:, 3] - df.iloc[:, 1]
+    df['difference_2_4'] = df.iloc[:, 4] - df.iloc[:, 2]
+
+    df = add_reference_columns(df, 18, 'difference_1_3', 'refX')  # For 'difference_1_3' at 6 PM
+    df = add_reference_columns(df, 18, 'difference_2_4', 'refY')  # For 'difference_2_4' at 6 PM
+
+    # Drop intermediate columns if necessary
+    df.drop(columns=['time'], inplace=True)
+    return df
+
+
+def smooth_data(df, window=5):
+    for column in df.select_dtypes(include=[np.number]).columns:
+        df[f'smoothed_{column}'] = df[column].rolling(window=window).mean()
+    return df
+
+
+def split_train_test(df):
+    unique_dates = np.sort(df['date'].unique())
+    last_date = unique_dates[-1]
+    day_before_last = unique_dates[-2]
+
+    # Define the cutoff timestamp: 6 PM of the day before the last date
+    cutoff_timestamp = pd.Timestamp(year=day_before_last.year, month=day_before_last.month,
+                                    day=day_before_last.day, hour=18, minute=0, second=0)
+
+    # Split the data based on the cutoff timestamp
+    train_df = df[df['timestamp'] <= cutoff_timestamp].copy()
+    test_df = df[df['timestamp'] > cutoff_timestamp].copy()
+
+    return train_df, test_df
+
+
+def get_column_list(df):
+    """
+    Return a list of column names from the DataFrame,
+    skipping the first seven and the last one.
+
+    Parameters:
+        df (pd.DataFrame): The DataFrame containing the data.
+
+    Returns:
+        col_list (list): The list of relevant column names.
+    """
+    col_list = df.columns.tolist()
+    items_to_remove = ['timestamp', 'Reference Mirror X-Motion', 'Reference Mirror Y-Motion', 'Motorized Mirror X-Motion',
+                       'Motorized Mirror Y-Motion', 'Differential X-Motion', 'Differential Y-Motion',
+                       'difference_1_3', 'difference_2_4', 'date']
+    col_list = [item for item in col_list if item not in items_to_remove]
+    return col_list
+
+
+def filter_dataframe_by_hours(df, start_hour, end_hour):
+    if start_hour < end_hour:
+        df_filtered = df[(df['timestamp'].dt.hour >= start_hour) & (df['timestamp'].dt.hour < end_hour)]
+    else:
+        df_filtered = df[(df['timestamp'].dt.hour >= start_hour) | (df['timestamp'].dt.hour < end_hour)]
+    return df_filtered
+
+
+def add_reference_columns(df, hour, col_name, new_col_name):
+    # Ensure the 'date' and 'time' columns are present
+    df['date'] = df['timestamp'].dt.date
+    df['time'] = df['timestamp'].dt.time
+
+    # Convert hour to a time object
+    reference_time = pd.to_datetime(f'{hour:02d}:00:00').time()
+
+    # Filter for records at or after the specified hour
+    df_after_hour = df[df['time'] >= reference_time]
+
+    # Get the first record for each date at or after the specified hour
+    df_first_after_hour = df_after_hour.groupby('date').first().reset_index()
+
+    # Keep only the relevant column and rename it for merging
+    df_first_after_hour = df_first_after_hour[['date', col_name]]
+    df_first_after_hour.rename(columns={col_name: f'{col_name}_at_6'}, inplace=True)
+    df_first_after_hour[f'{col_name}_at_6_yesterday'] = df_first_after_hour[f'{col_name}_at_6'].shift(1)
+
+    # Merge the new column with the original DataFrame
+    new_df = pd.merge(df, df_first_after_hour, on='date', how='left')
+    # If the column is still vacant, fill it with the last available value in that column
+    new_df[f'{col_name}_at_6'] = new_df[f'{col_name}_at_6'].ffill()
+    new_df[f'{col_name}_at_6_yesterday'] = new_df[f'{col_name}_at_6_yesterday'].ffill()
+
+    # Determine which reference value to use for each row
+    new_df[new_col_name] = new_df.apply(
+        lambda row: row[f'{col_name}_at_6_yesterday'] if row['time'] < reference_time else row[f'{col_name}_at_6'], axis=1
+    )
+
+    # Drop intermediate columns if necessary
+    new_df.drop(columns=[f'{col_name}_at_6', f'{col_name}_at_6_yesterday'], inplace=True)
+
+    return new_df
+
+def fit_and_predict_linear_eq(toggle_value, training_df, col, options):
+    # Create a mapping dictionary from value to label
+    value_to_label = {option['value']: option['label'] for option in options}
+
+    # Precompute the column names you'll need, including the target column to ensure alignment
+    col_names = [f'smoothed_{value_to_label[value]}' for value in toggle_value] + [col]
+
+    # Select the relevant columns from 'training_df' and drop rows with any NaN values to ensure alignment
+    df_selected = training_df[col_names].dropna()
+
+    # Separate X and y after ensuring they are aligned
+    X = df_selected[col_names[:-1]].to_numpy()
+    y = df_selected[col].values.reshape(-1, 1)
+
+    # Fit the linear equation
+    model = LinearRegression()
+    model.fit(X, y)
+
+    # Predict the values of y (target variable)
+    y_pred = model.predict(X).ravel()  # Flatten the array
+
+    # Calculate RMSE
+    rmse = mean_squared_error(y, y_pred, squared=False)
+
+    # Calculate the maximum error
+    max_err = max_error(y, y_pred)
+
+    # rmse = mean_squared_error(y, y_pred, squared=False)
+
+    return y_pred, rmse, max_err, model.coef_, model.intercept_
